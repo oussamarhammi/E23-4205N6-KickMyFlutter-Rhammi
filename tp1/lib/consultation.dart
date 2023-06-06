cd87875dfb5b44199b676228c516e510ca330c57
oussama <1876469@cegepmontpetit.ca>
2023-06-06 17:55:20 -0400

    fix: reconfiguration page consultation

--- tp1/lib/consultation.dart
+++ tp1/lib/consultation.dart
@@ -56,66 +56,81 @@ class _ConsultationState extends State<Consultation> {
       appBar: AppBar(
         title: Text('Consultation'),
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(child: Text("Nom: " + taskDetailResponse.name)),
-            Container(
-                child: Text("date d'échéance: " +
-                    taskDetailResponse.deadline.toString())),
-            Container(
-                child: Text("Avancement: " +
-                    taskDetailResponse.percentageDone.toString())),
-            Container(
-                child: Text("temps écoulé: " +
-                    taskDetailResponse.percentageTimeSpent.toString())),
-            Container(
+      body:
+      Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
                 child: Column(
-              children: [
-                Slider(
-                  min: 0.0,
-                  max: 100.0,
-                  value: _value,
-                  onChanged: (value) {
-                    setState(() {
-                      _value = value;
-                    });
-                  },
+                  children: [
+                    Container(child: Text("Nom: " + taskDetailResponse.name,style: TextStyle(fontWeight: FontWeight.bold))),
+                    Container(
+                        child: Text("date d'échéance: " +
+                            taskDetailResponse.deadline.toString(),style:TextStyle(fontWeight: FontWeight.bold)),
+                      padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    ),
+                    Container(
+                        child: Text("Avancement: " + taskDetailResponse.percentageDone.toString()+ " %",
+                            style:TextStyle(fontWeight: FontWeight.bold)),
+                      padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    ),
+                    Container(
+                        child: Text("temps écoulé: " + taskDetailResponse.percentageTimeSpent.toString()+ " jours"
+                            ,style:TextStyle(fontWeight: FontWeight.bold))
+                        ,padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    ),
+                    Container(
+                        child: Column(
+                          children: [
+                            Slider(
+                              min: 0.0,
+                              max: 100.0,
+                              value: _value,
+                              onChanged: (value) {
+                                setState(() {
+                                  _value = value;
+                                });
+                              },
+                            ),
+                            Text(_value.toStringAsFixed(0))
+                          ],
+                        )),
+                    Container(
+                      width: double.infinity,
+                      child: TextButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                        ),
+                        child: Text("Modifier "),
+                        onPressed: () {
+                          progress(widget.id, _value.toInt());
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Accueil()),
+                          );
+                          setState(() {});
+                        },
+                      ),
+                      padding: EdgeInsets.fromLTRB(32, 0, 32, 0),
+                    ),
+                  ],
                 ),
-                Text(_value.toStringAsFixed(0))
-              ],
-            )),
-            Container(
-              width: double.infinity,
-              child: TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                padding: EdgeInsets.fromLTRB(32, 0, 32, 0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(4)),
+                  shape: BoxShape.rectangle,
+                  border: Border.all(
+                    color: Colors.blue,
+                    width: 4,
+                  ),
                 ),
-                child: Text("Modifier "),
-                onPressed: () {
-                  progress(widget.id, _value.toInt());
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Accueil()),
-                  );
-                  setState(() {});
-                },
               ),
-              padding: EdgeInsets.fromLTRB(32, 0, 32, 0),
-            ),
-          ],
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(4)),
-          shape: BoxShape.rectangle,
-          border: Border.all(
-            color: Colors.blue,
-            width: 4,
+            ],
           ),
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
         ),
-      ),
     );
   }
 }
